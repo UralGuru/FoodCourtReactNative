7792e649d371c96bd87951a96fe816d7f864fc20
UralGuru <gizzatullinu@gmail.com>
2022-11-16 12:59:45 +0500

    Corrected auth.service.ts

--- api/auth.service.ts
+++ api/auth.service.ts
@@ -1,73 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
-type UserRegistration = {
-    email: string,
-    phone: string,
-    name: string,
-    password: string
+const API_URL = "http://food-court.tk/api/v1.0/";
+// const API_URL = "https://192.168.0.200:7777/v1.0/";
+
+const register = (name, phone, email, password) => {
+    return axios.post(API_URL + "auth/registration", {
+        name,
+        phone,
+        email,
+        password,
+    });
 };
 
-type UserLogin = {
-    email: string,
-    password: string
+const login = ({email, password}) => {
+    console.log(email, password)
+    return axios
+        .post(API_URL + "auth/login", {
+            email,
+            password
+        })
+        .then((response) => {
+            if (response.data.isSuccess) {
+                // localStorage.setItem("user", JSON.stringify(response.data));
+                console.log(response.data)
+            }
+            return response.data;
+        });
 };
 
-type GetUsersResponse = {
-    data: UserRegistration[];
+const logout = () => {
+    localStorage.removeItem("user");
+    return axios.post(API_URL + "signout").then((response) => {
+        return response.data;
+    });
 };
 
-const tokenAPI = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRqc2hwbGlzQGdtYWlsLmNvbSIsInN1YiI6ImM2NWM2MDRmLTE5M2YtNDhiZS05ZDhiLWE5YTUxMWUzNGNhNiIsInJvbGVzIjoidXNlciIsImV4cCI6MTY3MDQ5MTIxNSwiaXNzIjoiRm9vZGNvdXJ0QXBwLkFQSSIsImF1ZCI6IkZvb2Rjb3VydEFwcC5DbGllbnQifQ.L213BhjBjAtRtaLQSrBCOFau2A1JlIJAi37uzNC3RyE`;
-const API_URL = "http://51.250.9.6:8080/v1.0/";
+const getCurrentUser = () => {
+    // return JSON.parse(localStorage.getItem("user"));
+    return "getCurrentUser"
+};
 
-const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'http://51.250.9.6:8080/v1.0/',
-    headers: {
-        Authorization: `Bearer ${tokenAPI}`
-    }
-});
-
-class AuthService {
-    login(formValue: UserLogin) {
-        return axios
-            .post(API_URL + "auth/login", formValue)
-            .then(response => {
-                if (response.data.accessToken) {
-                    console.log(response.data.accessToken)
-                    // localStorage.setItem("user", JSON.stringify(response.data));
-                }
-
-                console.log(response.data);
-            });
-    }
-
-    logout() {
-        localStorage.removeItem("user");
-    }
-
-    // register(username: string, email: string, password: string) {
-    //     return axios.post(API_URL + "signup", {
-    //         username,
-    //         email,
-    //         password
-    //     });
-    // }
-
-    // register( email: string, password: string, name: string, phone: string) {
-    register( formValue: UserRegistration) {
-        console.log(formValue);
-        return instance.post("auth/registration", formValue)
-            .then((response) => console.log(response.data))
-            .catch(error => console.log(error))
-            .finally(() => console.log('...'))
-    }
-
-    getCurrentUser() {
-        const userStr = localStorage.getItem("user");
-        if (userStr) return JSON.parse(userStr);
-
-        return null;
-    }
+const AuthService = {
+    register,
+    login,
+    logout,
+    getCurrentUser,
 }
 
-export default new AuthService();
+export default AuthService;
